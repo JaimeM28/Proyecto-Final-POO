d88dd5a7e168cc6c6deadb91fe072f580ff956bb
Anikey <anikeyandrea20@gmail.com>
2023-01-07 00:38:01 -0600

    Biblioteca

--- Biblioteca.java
+++ Biblioteca.java
@@ -2,6 +2,15 @@ import Usuarios.Administrador;
 
 public class Biblioteca {
     public static void main(String[] args) {
+                Scanner sc =new Scanner(System.in);
+        
+        System.out.println("\t-------------------------");
+        System.out.println("\t|                        |");
+        System.out.println("\t|  B I E N V E N I D O   |");
+        System.out.println("\t|                        |");
+        System.out.println("\t-------------------------");
+
+
         Administrador prueba = new Administrador("Jaime", "hola123", "Jaime", "Miranda" ,18);
         prueba.registrarLibro();
     }
